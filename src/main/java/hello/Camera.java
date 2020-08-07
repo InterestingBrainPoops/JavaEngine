9bf4e03b49fb80db4c2d2c9d324919b04e0f7931
InterestingBrainPoops <satasiyahet@gmail.com>
2020-08-07 13:23:55 -0700

    Added Camera Class
    for scene class setup, not in mood to do math,
     but that needs to be done.

---
+++ src/main/java/hello/Camera.java
@@ -0,0 +1,26 @@
+package hello;
+public class Camera{
+    private int[] res;
+    private float focusLen;
+    private float[] dir;
+    public Camera(int[] Resolution, float focalLength, float[] direction){
+        res = Resolution;
+        focusLen = focalLength;
+        dir = direction;
+    }
+    public int[] getRes(){
+        return res;
+    }
+    public float getFocal(){
+        return focusLen;
+    }
+    public void setFocal(float newFocal){
+        focusLen = newFocal;//need to add the update clause to update the focal point itself, but I'm not in the mood to do the math.
+    }
+    public float[] getDirection(){
+        return direction;
+    }
+    public void setDirection(float[] newDirection){
+        dir = newDirection; // same here
+    }
+}
